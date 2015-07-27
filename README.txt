55d3686d0c594c7656799523c0f842a8b13b89e5
TonyZ <zjt1114@gmail.com>
2015-07-27 09:03:57 +0800

    Update some files

--- README.txt
+++ README.txt
@@ -1,1 +1,4 @@
- Copyright (C) 1979-2100 Tony ZHOU
+WebTools in Python by Tony ZHOU
+
+* The best way to be better in programming is to write some every day.
+* And there's nothing else can tell more about a programmer than the programs he has written.
--- src/copycat.py
+++ src/copycat.py
@@ -23,37 +23,39 @@ from content_filter import filter_content
 from simple_filter import SimpleFilter
 import logging, sys
 
+
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
-    
-    linkCollector = LinkCollector()
-    links = linkCollector.collect('http://javarevisited.blogspot.sg/', 'href="(http://javarevisited.blogspot.com/[0-9][0-9][0-9][0-9]/[0-9][0-9]/.+?.html)')
-    
+
+    link_collector = LinkCollector()
+    links = link_collector.collect('http://javarevisited.blogspot.sg/',
+                                  'href="(http://javarevisited.blogspot.com/[0-9][0-9][0-9][0-9]/[0-9][0-9]/.+?.html)')
+
     logging.info('Total articles to copy: {}'.format(str(len(links))))
-    
-    bloggerService = BloggerService()
-#     bloggerService.delete_all_posts()
-    
+
+    blogger_service = BloggerService()
+    #     bloggerService.delete_all_posts()
+
     simple_filter = SimpleFilter('CrawledLinks.txt')
-    
+
     for link in links:
         try:
             filtered_link = simple_filter.filter(link)
-            
+
             if filtered_link:
-                contentGenerator = ContentGenerator(filtered_link)
-                
-                title = contentGenerator.generate_readable_title()
-                content = filter_content(contentGenerator.generate_readable_article())
-                
-                bloggerService.create_post(title, content, False)
+                content_generator = ContentGenerator(filtered_link)
+
+                title = content_generator.generate_readable_title()
+                content = filter_content(content_generator.generate_readable_article())
+
+                blogger_service.create_post(title, content, False)
         except (ValueError, TypeError) as e:
             logging.error('Bypassing: {}'.format(str(e)))
             simple_filter.remove(link)
         except:
             logging.error('Bypassing unexpected error: {}'.format(str(sys.exc_info()[0])))
             simple_filter.remove(link)
-            
+
     simple_filter.record()
 
 
