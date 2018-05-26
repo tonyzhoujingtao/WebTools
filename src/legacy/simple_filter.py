f4387041f4e22f2cc4562315b37e3ecac10dfcf2
Tony Zhou <tony.zhou@getcruise.com>
2018-05-26 11:48:53 -0700

    MOVE: legacy programs in a separate folder

--- src/legacy/simple_filter.py
+++ src/legacy/simple_filter.py
@@ -2,8 +2,8 @@
 
 __author__ = 'tony.zjt.test@gmail.com (Tony ZHOU)'
 
-import logging
 import files
+import logging
 
 
 class SimpleFilter:
@@ -11,36 +11,36 @@ class SimpleFilter:
         self.filter_file = files.touch_open(filter_file_name, "r+")
         self.existing_items = set(self.filter_file.read().splitlines())
         print "Current existing items: {}", len(self.existing_items)
-    
+
     def filter(self, item):
         if item not in self.existing_items:
             self.existing_items.add(item)
             return item
         else:
             return ''
-        
+
     def remove(self, item):
         self.existing_items.remove(item)
-            
+
     def record(self):
         print 'Writing out all {} existing items to file ...', len(self.existing_items)
         for item in self.existing_items:
             self.filter_file.write(item + "\n")
         self.filter_file.close()
         print 'Writing out all {} existing items to file ... OK', len(self.existing_items)
-        
+
     def __del__(self):
         self.record()
-            
+
 
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
-    
+
     items = ('one', 'One', 'one', 'two')
     simple_filter = SimpleFilter()
     for item in items:
         filtered_item = simple_filter.filter(item)
         logging.info(filtered_item)
-    
+
 if __name__ == '__main__':
     main()
