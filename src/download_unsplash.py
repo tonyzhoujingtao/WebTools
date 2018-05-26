c44d0a96aeac1146abdc5b261922acb032b5e6f5
Tony Zhou <tony.zhou@getcruise.com>
2018-05-26 13:29:56 -0700

    Develop skeleton for download_unsplash_html

--- src/download_unsplash.py
+++ src/download_unsplash.py
@@ -6,26 +6,53 @@
 Photo hyperlink pattern: https://unsplash.com/photos/<.+>/download?force=true
 
 
-# Algorithm
+# TODO:
+    * Download the html of https://unsplash.com/ to /tmp/unsplash.html
 
-* Download the html of https://unsplash.com/ to /tmp/unsplash.html
+    * Crawl all the non-photo hyperlinks in unsplash.html and save them together
+    with https://unsplash.com/ to /tmp/non_photo_links.txt
 
-* Crawl all the non-photo hyperlinks in unsplash.html and save them together
-with https://unsplash.com/ to /tmp/non_photo_links.txt
+    * Grep all the photo hyperlinks in the HTMLs of each of the non_photo_links
+    and save to /tmp/photo_links.txt
 
-* Grep all the photo hyperlinks in the HTMLs of each of the non_photo_links
-and save to /tmp/photo_links.txt
-
-* Download all the photos to /tmp/photo_links.txt to /tmp/unsplash
+    * Download all the photos to /tmp/photo_links.txt to /tmp/unsplash
 """
 
 import logging
 
 
+def download_unsplash_html(path='/tmp/'):
+    """Download the html of https://unsplash.com/ to /{path}/unsplash.html.
+
+    Args:
+        path (str): The path to save the downloaded html to.
+
+    Returns:
+        str: the full path of the downloaded html.
+
+    Side effect:
+        I/O:
+            1. Web download an html
+            2. Save the html to local file system
+    """
+    hyperlink = 'https://unsplash.com/'
+    filename = 'unsplash.html'
+    full_path = path + filename
+
+    logging.info("downloading unsplash html from %s to %s ...",
+                 hyperlink, path)
+
+    logging.info("downloading unsplash html from %s to %s ... done",
+                 hyperlink, path)
+
+    return full_path
+
+
 def main():
     """Bootstrap."""
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
-    logging.info("Hello unsplash")
+
+    download_unsplash_html()
 
 
 if __name__ == "__main__":
