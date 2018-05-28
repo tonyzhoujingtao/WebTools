f0c6fea9c443d880233ddac54909a14e8c212557
Tony ZHOU <zjt1114@gmail.com>
2018-05-28 12:52:22 -0700

    ADD: crawl and download all photos

--- src/download_unsplash.py
+++ src/download_unsplash.py
@@ -7,23 +7,17 @@ Photo hyperlink pattern: https://unsplash.com/photos/<.+>/download?force=true
 
 
 # TODO:
-    * Download photos to /tmp/unsplash from urls in /tmp/photo_urls.txt.
-
-    * Delete /tmp/photo_urls.txt.
-
     * Save all non_photo_urls to /tmp/non_photo_urls.txt.
 
     * Repeatly crawl all the urls in /tmp/non_photo_urls.txt until it's empty.
-
-# DONE:
-    * Download the html of https://unsplash.com/.
-    * Grep all the photo urls from the downloaded html.
 """
 
 import contextlib
 import logging
+import os
 import re
 import urllib2
+import uuid
 
 USER_AGENT_VALUE = 'Magic Browser'
 USER_AGENT = 'User-Agent'
@@ -33,62 +27,105 @@ def shorten(long_string):
     return '({} ...)'.format(long_string[:10])
 
 
-def grep_photo_urls(html):
-    """Grep all photo urls from unsplash.html.
+def grep_urls(html):
+    logging.info('Grepping urls from %s ...', shorten(html))
 
-    Args:
-        path (str): The path to save photo_urls.txt to.
+    pattern = re.compile('\\shref=\"(\\S+)\"\\s')
 
-    Returns:
-        photo_urls (list).
-    """
+    all_hrefs = set()
+    for href in re.findall(pattern, html):
+        if href.startswith('http'):
+            all_hrefs.add(href)
+        else:
+            all_hrefs.add('https://unsplash.com'+href)
 
-    logging.info("Grepping photo urls from %s ...", shorten(html))
+    unsplash_hrefs = [x for x in all_hrefs if x.startswith('https://unsplash')]
+    unsplash_hrefs = [x for x in unsplash_hrefs if 'login_view' not in x]
+    unsplash_hrefs = [x for x in unsplash_hrefs if 'recover/initiate' not in x]
 
-    p = re.compile('https://unsplash.com/photos/\\S+/download\\?force=true')
-    photo_urls = re.findall(pattern=p, string=html)
+    photo_urls = set()
+    non_photo_urls = set()
 
-    logging.info("Grepping photo urls from %s ... done", shorten(html))
+    for h in unsplash_hrefs:
+        if h.startswith('https://unsplash.com/photos/') and h.endswith('download?force=true'):
+            photo_urls.add(h)
+        else:
+            non_photo_urls.add(h)
 
-    return photo_urls
+    logging.info('Grepping urls from %s ... done\n', shorten(html))
+
+    return photo_urls, non_photo_urls
 
 
-def download_unsplash_html():
-    """Download the html of https://unsplash.com/ to /{path}/unsplash.html.
-
-    Args:
-        path (str): The path to save the downloaded html to.
-
-    Returns:
-        html (str): unsplash.html in string.
-
-    Side effect:
-        I/O:
-            1. Web download an html
-    """
-    url = 'https://unsplash.com/'
-
-    logging.info("Downloading unsplash html from %s ...", url)
+def download_html(url):
+    logging.info('Downloading html from %s ...', url)
 
     request = urllib2.Request(url, headers={USER_AGENT: USER_AGENT_VALUE})
 
     html = ""
-    with contextlib.closing(urllib2.urlopen(request)) as wallpaper_file:
-        html += wallpaper_file.read()
+    with contextlib.closing(urllib2.urlopen(request)) as html_file:
+        html += html_file.read()
 
-    logging.info("Downloading unsplash html from %s ... done", url)
+    logging.info('Downloading html from %s ... done\n', url)
 
     return html
 
 
+def grep_file_name(url):
+    pattern = re.compile('.+photos/(\\S+)/download\\?force=true')
+    names = re.findall(pattern, url)
+    if names:
+        return names[0]
+    return uuid.uuid4().hex
+
+
+def download(url, path):
+    local_filename = path + grep_file_name(url) + '.jpg'
+
+    logging.info('Downloading %s to %s ...', url, local_filename)
+
+    request = urllib2.Request(url, headers={USER_AGENT: USER_AGENT_VALUE})
+
+    with contextlib.closing(urllib2.urlopen(request)) as wallpaper_file:
+        with open(local_filename, 'wb') as local_file:
+            local_file.write(wallpaper_file.read())
+
+    logging.info('Downloading %s to %s ... done', url, local_filename)
+
+
+def download_all(photo_urls, path='/tmp/unsplash/'):
+    logging.info('Downloading %d photos ...', len(photo_urls))
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for url in photo_urls:
+        download(url, path)
+
+    logging.info('Downloading %d photos ... done\n', len(photo_urls))
+
+
 def main():
-    """Bootstrap."""
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-    html = download_unsplash_html()
-    photo_urls = grep_photo_urls(html)
-    logging.info(photo_urls)
-    logging.info(len(photo_urls))
+    non_photos_new = set(['https://unsplash.com/'])
+    non_photos_visited = set()
+
+    photos_visited = set()
+
+    while non_photos_new:
+        non_photo_url = non_photos_new.pop()
+        non_photos_visited.add(non_photo_url)
+
+        html = download_html(non_photo_url)
+
+        photo_urls, non_photo_urls = grep_urls(html)
+        download_all(photo_urls - photos_visited)
+        photos_visited |= photo_urls
+
+        non_photos_new |= (non_photo_urls - non_photos_visited)
+
+    logging.info('Downloaded %d images', len(non_photos_visited))
 
 
 if __name__ == "__main__":
